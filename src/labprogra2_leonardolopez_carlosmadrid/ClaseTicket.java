d8ead19658a0129eae55ad51c0b04ff9e7be74d5
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-07-25 10:14:01 -0600

    Creacion de funciones en clase PalindromoAir

--- src/labprogra2_leonardolopez_carlosmadrid/ClaseTicket.java
+++ src/labprogra2_leonardolopez_carlosmadrid/ClaseTicket.java
@@ -1,34 +1,47 @@
-
 package labprogra2_leonardolopez_carlosmadrid;
 
 public class ClaseTicket {
+
     private String nombrePasajero;
     private double montoFinalT;
     private double montoOriginalT;
     private boolean isPalindromo;
-    
-    public ClaseTicket(String nombrePasajero, double montoFinalT){
+
+    public ClaseTicket(String nombrePasajero, double montoOriginalT, boolean isPalindromo) {
         this.nombrePasajero = nombrePasajero;
+        this.montoOriginalT = montoOriginalT;
+        this.isPalindromo = isPalindromo;
+
+        if (isPalindromo) {
+            montoFinalT = montoOriginalT * 0.8;
+
+        } else {
+            montoFinalT = montoOriginalT;
+        }
+
     }
-    
-    public String getName(){
+
+    public String getName() {
         return nombrePasajero;
     }
-    
-    public double getFinalAmount(){
+
+    public double getFinalAmount() {
         return montoFinalT;
     }
-    
-    public double OriginalAmount(){
+
+    public double OriginalAmount() {
         return montoOriginalT;
     }
-    
-    public boolean isPalindrome(){
+
+    public boolean isPalindrome() {
         return isPalindromo;
     }
     
-    public void print(){
-        
+    public String print(){
+        return "Nombre Pasajero: " + nombrePasajero + "\n" +
+                "Monto Original: " + montoOriginalT + "\n" +
+                "Monto Final: " + montoFinalT + "\n" +
+                "Es Palindromo: "+ (isPalindromo ? "Si" : "No");
     }
-    
+
 }
--- src/labprogra2_leonardolopez_carlosmadrid/PalindromoAir.java
+++ src/labprogra2_leonardolopez_carlosmadrid/PalindromoAir.java
@@ -1,6 +1,61 @@
-
 package labprogra2_leonardolopez_carlosmadrid;
 
 public class PalindromoAir {
+
+    private static ClaseTicket asientos[];
+
+    public PalindromoAir() {
+        asientos = new ClaseTicket[30];
+    }
+
+    public int firstAvailable(int i) {
+        if (i >= asientos.length) {
+            return -1; //Esto significa que no encontró asientos
+        }
+        if (asientos[i] == null) {
+            return i;
+        }
+
+        return firstAvailable(i + 1); //Sigue buscando si hay más asientos
+    }
     
+    public int searchPassenger(String name, int i){
+        if(i>=asientos.length){
+            return -1; //Si no se encontró el pasajero en los asientos
+        }
+        
+        if(asientos[i] != null && asientos[i].getName().equalsIgnoreCase(name)){
+            return i; //Devuelve el asiento
+        }
+       
+        return searchPassenger(name, i + 1); //Si no lo encuentra busca con el mismo nombre, pero diferente asiento.
+    }
+    
+    private static boolean isPalindromoDos(String nombrePasajero, int inicio, int fin){
+       
+        if(inicio >= fin){
+            return true;
+        }
+        if(nombrePasajero.charAt(inicio) !=  nombrePasajero.charAt(fin)){
+            return false;
+        }
+        
+        return isPalindromoDos(nombrePasajero, inicio+1, fin - 1);
+    }
+   
+    public static boolean isPalindromo(String nombrePasajero){
+        String nombreMinus = nombrePasajero.toLowerCase();
+        return isPalindromoDos(nombreMinus, 0, nombrePasajero.length() -1);
+    }
+            
+            
+    
+    public static void main(String[] args) {
+        
+        if(isPalindromo("Fernando")){
+            System.out.println("Es palindromo");
+        }else{
+            System.out.println("No es palindromo");
+        }
+    }
 }
