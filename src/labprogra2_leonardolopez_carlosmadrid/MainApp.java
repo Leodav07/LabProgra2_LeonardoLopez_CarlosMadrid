d9849b82add3162f5c960cc2d81777b8cad2873b
ftMadrid <fernandomadridcv04@gmail.com>
2025-07-25 10:21:43 -0600

    Agregados botones y espacio de texto

--- src/labprogra2_leonardolopez_carlosmadrid/MainApp.java
+++ src/labprogra2_leonardolopez_carlosmadrid/MainApp.java
@@ -1,17 +1,67 @@
 package labprogra2_leonardolopez_carlosmadrid;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class MainApp {
     
     public static void main(String[] args) {
         
         JFrame frame = new JFrame();
-        frame.setSize(600, 600);
+        JLabel label = new JLabel("Nombre del Pasajero");
+        JTextField pasajero = new JTextField();
+        JButton sellticket = new JButton("Sell Ticket");
+        JButton cancelticket = new JButton("Cancel Ticket");
+        JButton dispatch = new JButton("Dispatch");
+        JButton prtpassengers = new JButton("Print Passengers");
+        JButton vwincomes = new JButton("View Incomes");
+        JButton srcpassengers = new JButton("Search Passengers");
+        
+        frame.setLayout(null);
+        frame.setSize(1000, 600);
         frame.setTitle("Recursividad");
+        frame.setLocationRelativeTo(null);
+        
+        label.setBounds(650, -50, 300, 250);
+        label.setFont(new Font("Kefa", Font.BOLD, 24));
+        
+        pasajero.setBounds(648, 90, 200, 35);
+        pasajero.setFont(new Font("Kefa", Font.PLAIN, 18));
+        
+        sellticket.setBounds(648, 140, 150, 50);
+        sellticket.setFont(new Font("Kefa", Font.BOLD, 20));
+        sellticket.setForeground(Color.GREEN);
+        
+        cancelticket.setBounds(648, 200, 150, 50);
+        cancelticket.setFont(new Font("Kefa", Font.BOLD, 20));
+        cancelticket.setForeground(Color.RED);
+        
+        dispatch.setBounds(648, 260, 150, 50);
+        dispatch.setFont(new Font("Kefa", Font.BOLD, 20));
+        dispatch.setForeground(Color.BLUE);
+        
+        prtpassengers.setBounds(800, 140, 150, 50);
+        prtpassengers.setFont(new Font("Kefa", Font.BOLD, 16));
+        prtpassengers.setForeground(Color.MAGENTA);
+        
+        vwincomes.setBounds(800, 200, 150, 50);
+        vwincomes.setFont(new Font("Kefa", Font.BOLD, 20));
+        vwincomes.setForeground(Color.ORANGE);
+        
+        srcpassengers.setBounds(800, 260, 150, 50);
+        srcpassengers.setFont(new Font("Kefa", Font.BOLD, 15));
+        srcpassengers.setForeground(Color.cyan);
+        
+        frame.add(label);
+        frame.add(pasajero);
+        frame.add(sellticket);
+        frame.add(cancelticket);
+        frame.add(prtpassengers);
+        frame.add(dispatch);
+        frame.add(vwincomes);
+        frame.add(srcpassengers);
         
         frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
         
     }
     
