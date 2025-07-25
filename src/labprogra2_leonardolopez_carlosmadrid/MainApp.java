e7818deb6bf77bfdb129399f61dfa077d8f987e5
ftMadrid <fernandomadridcv04@gmail.com>
2025-07-25 09:00:04 -0600

    Arreglado bud de clase main

--- src/labprogra2_leonardolopez_carlosmadrid/MainApp.java
+++ src/labprogra2_leonardolopez_carlosmadrid/MainApp.java
@@ -1,12 +1,17 @@
 package labprogra2_leonardolopez_carlosmadrid;
 
+import javax.swing.JFrame;
+
 public class MainApp {
     
     public static void main(String[] args) {
         
-        PantallaPrincipal pantalla = new PantallaPrincipal();
-        pantalla.setVisible(true);
-        pantalla.setLocationRelativeTo(null);
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+        frame.setTitle("Recursividad");
+        
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
         
     }
     
