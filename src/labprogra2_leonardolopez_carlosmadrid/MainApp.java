08b1f2de02076b7eb0cb2c82a7e1d6107d40dc8a
ftMadrid <fernandomadridcv04@gmail.com>
2025-07-25 08:57:38 -0600

    Arreglado bug de JDK

---
+++ src/labprogra2_leonardolopez_carlosmadrid/MainApp.java
@@ -0,0 +1,13 @@
+package labprogra2_leonardolopez_carlosmadrid;
+
+public class MainApp {
+    
+    public static void main(String[] args) {
+        
+        PantallaPrincipal pantalla = new PantallaPrincipal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+    
+}
